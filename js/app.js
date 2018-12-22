46577ee47ff17e2cc87f1b56b318221551015efb
Tom Barnes <barnes.thomas.c@gmail.com>
2018-12-22 14:24:20 -0500

    Update 2018-12-22T19:24:18.455Z

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// var experiment = document.getElementById("clickbtn")
+
+// experiment.addEventListener("click", function() {
+//     console.log("clickkkkkk");
+// })
+
+
+// if ('serviceWorker' in navigator) {
+
+//     navigator.serviceWorker
+//         .register('./service-worker.js', { scope: './' })
+//         .then(function(registration){
+//             console.log("Service Worker registered!");
+//         })
+//         .catch(function(err) {
+//             console.log("Service Worker failed to register", err)
+//         })
+
+// }
